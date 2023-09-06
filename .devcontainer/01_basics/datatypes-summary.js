31eb7686136d2650764de81377cf676285879583
DeeptiPandey <70426287+DeeptiPandey@users.noreply.github.com>
2023-09-06 16:51:55 +0000

    stacks and heaps

--- .devcontainer/01_basics/datatypes-summary.js
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -35,3 +35,10 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+//+++++++++++++++++++++++++++++++++++++++++++++++++++
+// Stack(Primitive) Heap(Non primitive)
+
+let myGitname ="DeeptiPandey"
+
+let anothername = myGitname
+console.log(anothername);
