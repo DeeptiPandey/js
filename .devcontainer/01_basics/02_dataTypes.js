996ca5669e6c1630535207152ef6da6c48eee8d5
DeeptiPandey <70426287+DeeptiPandey@users.noreply.github.com>
2023-09-06 12:42:42 +0000

    data types info

--- .devcontainer/01_basics/02_dataTypes.js
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Deepti")
 
 
-let name = "hitesh"
+let name = "Deepti"
 let age = 18
 let isLoggedIn = false
 let state;
